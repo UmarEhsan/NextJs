e869bc656d5efe8f1213357bdedec480e93cf965
Shafaque Qayyum <shafaque@zaavya.com>
2020-11-13 21:09:27 +0500

    create blog post Using next.js

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,20 +1,23 @@
-import Link from 'next/link'
+
 import Head from 'next/head'
-import Layout from '../../components/layout'
+import Layout, { siteTitle } from '../../components/layout.js'
+import utilStyles from '../../styles/utils.module.css'
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
 
 
-export default function FirstPost() {
-    return (
-        <Layout>
-            <Head>
-                <title>First Post</title>
-            </Head>
-            <h1>First Post</h1>
-            <h2>
-                <Link href="/">
-                    <a>Back to home</a>
-                </Link>
-            </h2>
-        </Layout>
-    )
+      <section className={utilStyles.headingMd}>
+        <p>I’m a full-stack JavaScript engineer, front-end focused, having almost half a decade of experience working on the web. I have experience working in startups. I am a fan of startup culture where everyone has a voice and gets to wear different hats every day. I enjoy having a variety of tasks and responsibilities and being able to jump from frontend to backend or DevOps when needed.</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  )
 }
