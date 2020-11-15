36efa0e59282e70c684aac27e26b03c3989cdb53
Ehsan Umar <shafaque@zaavya.com>
2020-11-15 07:39:26 +0500

    implement getstatic props in posts

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Link from 'next/link';
 
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <div className="container">
       <Head>
--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -2,22 +2,41 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../../components/layout.js'
 import utilStyles from '../../styles/utils.module.css'
+import { getSortedPostsData } from '../../lib/posts'
 
-export default function Home() {
+
+export default function Home({allPostsData}) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
 
-
-      <section className={utilStyles.headingMd}>
-        <p>I’m a full-stack JavaScript engineer, front-end focused, having almost half a decade of experience working on the web. I have experience working in startups. I am a fan of startup culture where everyone has a voice and gets to wear different hats every day. I enjoy having a variety of tasks and responsibilities and being able to jump from frontend to backend or DevOps when needed.</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+      <section className={utilStyles.headingMd}>…</section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
+}
+
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
